bdb6aeb630082a74518c91fad1ae528bc03d565b
Revant Chandanala <revant.chandanala296@gmail.com>
2014-03-19 14:51:20 -0400

    Update signup.php

--- Social-Networking-Website/signup.php
+++ Social-Networking-Website/signup.php
@@ -11,11 +11,11 @@ function checkUser(user)
         return
     }
 
-    params  = "user=" + user.value
+   
     request = new ajaxRequest()
     request.open("POST", "checkuser.php", true)
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-    request.setRequestHeader("Content-length", params.length)
+    
     request.setRequestHeader("Connection", "close")
     
     request.onreadystatechange = function()
