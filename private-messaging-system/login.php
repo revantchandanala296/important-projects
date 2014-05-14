eb68b5aaab6bbc0ef19b539cb1a60f00a7bc0e31
Revant Chandanala <revant.chandanala296@gmail.com>
2014-05-14 18:47:19 -0400

    Create login.php

---
+++ private-messaging-system/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form method='post' action='post.php'>
+    <label for="user">Username</label>
+    <input type='text' name='user' id='user'></br>
+    <label for="pass">Password</label>
+    <input type='password' name='pass' id='pass' /></br>
+    <input type='submit' name='submit' value='Login'>
+</form>
+</body>
+</html>
